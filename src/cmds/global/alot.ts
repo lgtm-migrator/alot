db93b55f3d2232d98ff00b0f16d61ddea858c9e1
adrifcastr <adrifcastr@gmail.com>
2021-04-25 17:00:27 +0200

    alot of debugging

---
+++ src/cmds/global/alot.ts
@@ -0,0 +1,18 @@
+import { CommandInteraction } from 'discord.js';
+import { Command } from 'src/@types/Util';
+
+export async function run(interaction: CommandInteraction): Promise<void> {
+    const url = '';
+    return interaction.reply(`[Invite me](<${url}>)`);       
+}
+
+export const info: Command['info'] = {
+    roles: [],
+    user_perms: [],
+    bot_perms: []
+};
+
+export const data: Command['data'] = {
+    name: 'add',
+    description: 'alot of oauth2 invite link'
+};
--- src/events/client/commandRefused.ts
+++
@@ -1,9 +0,0 @@
-import Util from '../../Util.js';
-import { CommandInteraction, TextChannel } from 'discord.js';
-
-export default {
-    name: 'commandRefused',
-    async run(interaction: CommandInteraction, reason: string): Promise<void> {
-        Util.log(`Command Refused:\n\n\`${interaction?.member?.user?.tag}\` attempted to use \`${interaction?.commandName}\`\nCommand failed due to: \`${reason}\`\nOrigin: \`#${(interaction?.channel as TextChannel).name}\` at \`${interaction?.guild?.name}\``);
-    }
-};
